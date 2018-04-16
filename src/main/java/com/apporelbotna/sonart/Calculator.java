5ec599455d1ef2c71dfaab46434a1149fed30f80
Jendoliver <alvarezmonllor@gmail.com>
2018-04-16 20:15:45 +0200

    Adding the clakes and test aka classic calculator

---
+++ src/main/java/com/apporelbotna/sonart/Calculator.java
@@ -0,0 +1,28 @@
+package com.apporelbotna.sonart;
+
+public class Calculator
+{
+	public static final String ZERO_DIVISION = "The divisort can't be zero!";
+
+	public int add(int a, int b)
+	{
+		return a + b;
+	}
+
+	public int sub(int a, int b)
+	{
+		return a - b;
+	}
+
+	public int mul(int a, int b)
+	{
+		return a * b;
+	}
+
+	public int div(int a, int b)
+	{
+		if(b == 0) throw new IllegalArgumentException(ZERO_DIVISION);
+		return a / b;
+	}
+
+}
---
+++ src/test/java/com/apporelbotna/sonart/CalculatorTest.java
@@ -0,0 +1,48 @@
+package com.apporelbotna.sonart;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class CalculatorTest
+{
+	private Calculator calculator = new Calculator();
+
+	@Test
+	public void addTest() throws Exception
+	{
+		assertEquals(3, calculator.add(2, 1));
+	}
+
+	@Test
+	public void subTest() throws Exception
+	{
+		assertEquals(1, calculator.sub(2, 1));
+	}
+
+	@Test
+	public void mulTest() throws Exception
+	{
+		assertEquals(2, calculator.mul(2, 1));
+	}
+
+	@Test
+	public void divTest() throws Exception
+	{
+		assertEquals(2, calculator.div(2, 1));
+	}
+
+	@Test
+	public void divPerZeroShouldThrowExceptionTest() throws Exception
+	{
+		try
+		{
+			assertEquals(2, calculator.div(2, 0));
+			fail();
+		}
+		catch(IllegalArgumentException e)
+		{
+			assertEquals(Calculator.ZERO_DIVISION, e.getMessage());
+		}
+	}
+}
